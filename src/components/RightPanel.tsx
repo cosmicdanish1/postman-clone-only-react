4e1d8c523d1ab57ba7222cc637c31b82fe2363e3
cosmicdanish1 <cosmicdanish@outlook.com>
2025-07-07 20:14:01 +0530

    Done  All

--- src/components/RightPanel.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const RightPanel: React.FC = () => (
-  <div className="w-80 bg-zinc-900 border-l border-zinc-800 flex items-center justify-center text-zinc-400 text-base">
-    Collections Panel (stub)
-  </div>
-);
-
-export default RightPanel; 
